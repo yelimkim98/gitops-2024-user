89d6523a6a05c4d6d5c0f4397c9332d3376ab77e
yelimkim98 <kiel0103@naver.com>
2024-12-26 17:40:23 +0900

    수정

--- src/main/java/naver/kiel0103/gitops2024user/controller/UserController.java
+++ src/main/java/naver/kiel0103/gitops2024user/controller/UserController.java
@@ -6,10 +6,12 @@ import naver.kiel0103.gitops2024user.domain.UserDomain;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -28,8 +30,17 @@ public class UserController {
         return ResponseEntity.ok(users);
     }
 
-    @GetMapping("/users/{id}")
-    public ResponseEntity<UserDomain> get(@PathVariable Long id) {
-        return ResponseEntity.ok(users.get(id));
+    @GetMapping("/user")
+    public ResponseEntity<UserDomain> get(
+            @RequestParam("username") String username,
+            @RequestParam("pw") String password
+    ) {
+        Optional<UserDomain> userOptional = users.values().stream()
+                .filter(user -> user.getUsername().equals(username)
+                        && user.getPassword().equals(password))
+                .findFirst();
+
+        return userOptional.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.ok(null));
     }
 }
